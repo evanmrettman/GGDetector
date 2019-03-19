3742e006ef9109f0ba1ee8a2ab0f1804dcf7d149
Evan Rettman <erettman@masonlive.gmu.edu>
2019-03-19 05:55:08 -0400

    initial main setup

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#import antigravity
+import utility.logging as log
+
+def main():
+    log.info("Hello from Steam Sensor")
+
+if __name__ == "__main__":
+    log.starting()
+    main()
+    log.ending()
